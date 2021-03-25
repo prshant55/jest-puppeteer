4a657093a8a5968a117bd4415618f41850d6cafa
prshant55 <72465599+prshant55@users.noreply.github.com>
2021-03-25 12:48:39 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+const { generateText, createElement, validateInput } = require('./util');
+
+const initApp = () => {
+  // Initializes the app, registers the button click listener
+  const newUserButton = document.querySelector('#btnAddUser');
+  newUserButton.addEventListener('click', addUser);
+};
+
+const addUser = () => {
+ // Fetches the user input, creates a new HTML element based on it
+ // and appends the element to the DOM
+ const newUserNameInput = document.querySelector('input#name');
+ const newUserAgeInput = document.querySelector('input#age');
+
+ const outputText = checkAndGenerate(
+   newUserNameInput.value,
+   newUserAgeInput.value
+ );
+
+ if (!outputText) {
+   return;
+ }
+
+// Start the app!
+initApp();
